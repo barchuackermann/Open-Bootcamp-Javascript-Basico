ea8d91c3329878101a05ff0da5e734e20bcb586b
BarbiAckermann <barbiackermann@gmail.com>
2023-08-28 00:08:55 +0200

    15

---
+++ Ejercicio15/index.js
@@ -0,0 +1,12 @@
+let nombre = "Bárbara"
+let apellido = "Ackermann"
+let nombreApellido = {
+  nombre: "Bárbara",
+  apellido: "Ackermann",
+}
+
+/* sessionStorage.setItem("Barchu", JSON.stringify(nombreApellido))
+localStorage.setItem("Barchu", JSON.stringify(nombreApellido))
+let fecha2min = new Date(Date.now() + 120000).toUTCString()
+document.cookie =
+  "Barchu=" + JSON.stringify(nombreApellido) + "; expires=" + fecha2min */
